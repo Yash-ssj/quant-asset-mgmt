52036f7691a36011c2ccfd7c6cda0fa679cbac9c
Yash-ssj <76586157+Yash-ssj@users.noreply.github.com>
2022-11-01 20:09:49 +0530

    Update bktst_utils.py

--- bktst_utils.py
+++ bktst_utils.py
@@ -1,2 +1,8 @@
-def generate_weights(no_of_assets, bktst_strt_dt, bktst_end_dt, rebal_frequency = '3M', weighting_scheme = 'EW'):
-  con_weights = [1.0000/no_of_assets] * no_of_assets
+from datetime import datetime
+
+def generate_weights(no_of_assets, bktst_strt_dt, bktst_end_dt, rebal_frequency = , weighting_scheme = 'EW'):
+  if weighting_scheme == 'EW':
+    cons_weights = [1.000000/no_of_assets] * no_of_assets
+  
+  return cons_weights
+
