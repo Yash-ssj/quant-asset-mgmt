afc9c2f349cdf68c6585c0cc9681b33348774d8d
Yash-ssj <76586157+Yash-ssj@users.noreply.github.com>
2022-11-02 00:51:51 +0530

    Update bktst_utils.py

--- bktst_utils.py
+++ bktst_utils.py
@@ -7,13 +7,15 @@ def generate_weights(no_of_assets, weighting_scheme = 'EW'):
   
   return cons_weights
 
-def price_direction_strategy(asset_price_series, price_dirs = [3, 5, 10, 20]):
+def price_direction_strategy(asset_price_series, price_dirs = [0, 5, 10, 15, 20]):
   ticker_selection = []
   for x in asset_price_series['Ticker'].unique().tolist():
     z = 0
-    for y in sorted(price_dirs):
-      qutils.get_price_direction(asset_price_history = asset_price_series[asset_price_series['Ticker']==x], no_of_days = y)
-      
+    for y in price_dirs:
+      z = z + qutils.get_price_direction(asset_price_history = asset_price_series[asset_price_series['Ticker']==x], no_of_days = 10, shift_days = y)
+    if z >= 5:
+      ticker_selection.append(x)
+  return ticker_selection
 
 def get_rebal_selections:
   
