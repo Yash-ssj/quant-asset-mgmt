88e56a90713a0168e2be99f2ccff63ffb503f49a
Yash-ssj <76586157+Yash-ssj@users.noreply.github.com>
2022-11-03 22:47:29 +0530

    Update bktst_utils.py

--- bktst_utils.py
+++ bktst_utils.py
@@ -19,10 +19,23 @@ def price_direction_strategy(asset_price_series, price_dirs = [0, 5, 10, 15, 20]
   return ticker_selection
 
 def calc_backtest_returns(asset_price_history, backtest_portfolio_alloc):
+  asset_price_history = qutils.get_price_returns(asset_price_history = asset_price_history)
+  calc_dates = sorted(asset_price_history['Date'].unique().tolist())
   
 
-def get_rebal_selections(backtest_strt_dt, backtest_end_dt, rebal_frequency, asset_perf_history, invstmt_strategy = 'price_direction'):
-  bktst_alloc = pd.DataFrame(columns = ['Date','Asset','Weight'])
-  rebal_dts = [datetime.strptime(backtest_strt_dt, '%Y-%m-%d') + ]
-  
+def get_rebal_selections(backtest_strt_dt, backtest_end_dt, rebal_frequency, asset_perf_history, invstmt_strategy = 'price_direction', weight_scheme = 'EW'):
+  bktst_alloc = pd.DataFrame(columns = ['Date','Ticker','Weight'])
+  #no_of_rebals = (datetime.strptime(backtest_end_dt, '%Y-%m-%d') - datetime.strptime(backtest_strt_dt, '%Y-%m-%d'))
+  #rebal_dts = [datetime.strptime(backtest_strt_dt, '%Y-%m-%d') + relativedelta(months = rebal_frequency) * i for i in [0,]]
+  rebal_date = backtest_strt_dt
+  while rebal_date <= backtest_end_dt:
+    rebal_selections = price_direction_strategy(asset_perf_history)
+    col1 = [rebal_date]*len(rebal_selections)
+    bktst_alloc_dict = {"Date":[rebal_date]*len(rebal_selections), "Ticker":rebal_selections, "Weight": generate_weights(no_of_assets = len(rebal_selections), weighting_scheme = weight_scheme)}
+    bktst_alloc_df = pd.DataFrame(bktst_alloc_dict.items(), columns = bktst_alloc_dict.keys().tolist())
+    bktst_alloc = pd.concat([bktst_alloc, bktst_alloc_df],ignore_index=True)
+    rebal_date = [datetime.strptime(rebal_date, '%Y-%m-%d') + relativedelta(months = rebal_frequency)
+  return bktst_alloc
+    
+    
   
